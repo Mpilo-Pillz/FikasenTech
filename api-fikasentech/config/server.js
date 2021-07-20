b68dd3fe0b6f9d1c2e42339242f326b69ccd68f1
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-20 06:35:14 +0200

    initial commit with api

---
+++ api-fikasentech/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d50e136d85c1aeea3bbd767004a6407'),
+    },
+  },
+});
---
+++ api-fikasentech/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '73872e04-594e-4960-baf3-a4e1cb302c8d'
+};
