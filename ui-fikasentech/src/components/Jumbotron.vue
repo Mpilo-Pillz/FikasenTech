e285639d087121c131cc6465ae867a4fd1980be7
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-27 06:48:12 +0200

    ui - adding jumbotron

---
+++ ui-fikasentech/src/components/Jumbotron.vue
@@ -0,0 +1,32 @@
+<style lang="scss" scoped>
+.profile-pic {
+  position: relative;
+  border-radius: 50%;
+  height: 180px;
+  width: 180px;
+  padding: 0;
+  margin: 0;
+  border: 15px solid transparent;
+  z-index: 9999;
+  transition: all 0.3s ease-out;
+}
+</style>
+<template>
+  <section class="flex-center">
+    <img
+      class="profile-pic"
+      src="https://lh3.googleusercontent.com/pWGKsbC67uKkiC4xQKOPM4juc2DCUN9oqsBKqaqpRawSTFAeYfPTW-MhTnvFGEUJ3nMJoqsbY3x6rs_uqZb9nts9npWdjgUtI0W3jhDfYd9LuSa7DFj6q9R5apEXi0JyTiuSJqQV=w2400"
+      alt="fikasentani"
+    />
+    <h1>MT Fikasentani KN Dlamini</h1>
+    <h2>Front End Developer, Software Developer in Test</h2>
+    <button class="btn">Hire Me</button>
+    <button class="btn">Download CV</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Jumbotron",
+};
+</script>
--- ui-fikasentech/src/views/Home.vue
+++ ui-fikasentech/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Jumbotron />
     <Project />
   </div>
 </template>
@@ -7,10 +8,11 @@
 <script>
 // @ is an alias to /src
 import Project from "@/components/Project.vue";
-
+import Jumbotron from "@/components/Jumbotron.vue";
 export default {
   name: "Home",
   components: {
+    Jumbotron,
     Project,
   },
 };
