bc942ad8cb6b06f8e6f4e59fb9e4650d52c72291
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-29 08:05:31 +0200

    ui accing card to projectList page

--- ui-fikasentech/src/views/ProjectList.vue
+++ ui-fikasentech/src/views/ProjectList.vue
@@ -2,13 +2,18 @@
   <section>
     Projeccts
     {{ allProjects.length }}
+    <Card :items="allProjects" />
   </section>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
+import Card from "../components/Card.vue";
 export default {
   name: "ProjectList",
+  components: {
+    Card,
+  },
   computed: mapGetters(["allProjects"]),
   methods: mapActions(["fetchProjects"]),
   created() {
